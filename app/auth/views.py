1e1ff09e0e264f9edd19df9d7f135dad69e1ff13
toonow <toono@qq.com>
2016-04-06 22:53:35 +0800

    修改网页登录与注册页面

--- app/auth/views.py
+++ app/auth/views.py
@@ -34,8 +34,21 @@ def login():
         if user is not None and user.verify_password(form.password.data):
             login_user(user, form.remember_me.data)
             return redirect(request.args.get('next') or url_for('main.index'))
-        flash('Invalid username or password.')
+        flash('用户名或密码错误')
     return render_template('auth/login.html', form=form)
+
+
+# 旧的登录方法
+@auth.route('/oldlogin', methods=['GET', 'POST'])
+def oldlogin():
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(email=form.email.data).first()
+        if user is not None and user.verify_password(form.password.data):
+            login_user(user, form.remember_me.data)
+            return redirect(request.args.get('next') or url_for('main.index'))
+        flash('Invalid username or password.')
+    return render_template('auth/oldlogin.html', form=form)
 
 
 @auth.route('/logout')
@@ -55,12 +68,27 @@ def register():
                     password=form.password.data)
         db.session.add(user)
         db.session.commit()
+        flash('注册成功！')
+        return redirect(url_for('auth.login'))
+    return render_template('auth/register.html', form=form)
+
+
+# 旧的注册方法
+@auth.route('/oldregister', methods=['GET', 'POST'])
+def oldregister():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(email=form.email.data,
+                    username=form.username.data,
+                    password=form.password.data)
+        db.session.add(user)
+        db.session.commit()
         token = user.generate_confirmation_token()
         send_email(user.email, 'Confirm Your Account',
                    'auth/email/confirm', user=user, token=token)
         flash('A confirmation email has been sent to you by email.')
-        return redirect(url_for('auth.login'))
-    return render_template('auth/register.html', form=form)
+        return redirect(url_for('auth.oldlogin'))
+    return render_template('auth/oldregister.html', form=form)
 
 
 @auth.route('/confirm/<token>')
